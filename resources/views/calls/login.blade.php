6717ab5734c7d2a9ef0ae19684eb84a126a9e682
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-04-01 06:26:34 -0400

    cris
    algunas correcciones

--- resources/views/calls/login.blade.php
+++ resources/views/calls/login.blade.php
@@ -25,14 +25,16 @@
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
 						<input type="text" class="form-control" placeholder="username">
-						
+						  
 					</div>
+                    <small id="passwordHelpBlock" class="form-text text-muted"> oe!! tu usuario es tu CI #quedateencasa , contain letters and numbers , <br>and must not contain spaces, special characters, or emoji. </small>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
 						<input type="password" class="form-control" placeholder="password">
 					</div>
+                    <small id="passwordHelpBlock" class="form-text text-muted"> Your password must be 8-20 characters long, contain letters and numbers , <br>and must not contain spaces, special characters, or emoji. </small>
 					<div class="row align-items-center remember">
 						<input type="checkbox">Remember Me
 					</div>
