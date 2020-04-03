cf9142c1f42c85732d1599d52f0000589a8fa0de
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-04-03 06:47:17 -0400

    Update footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,54 @@
+<style>
+footer {
+	background-color:#1A1A1A;
+	height: 50%;
+  color: white;
+  padding: 15px;
+  
+}
+
+.main1 { 
+  display:flex;
+  margin:0 auto;
+}
+
+a img:hover {
+    color: #fff;
+}
+</style>
+
+
+<link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+<footer>
+  <div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-md-6 text-left">
+            <h6 class="text-muted lead">CONTACTO:</h6>
+            <h6 class="text-muted">
+            Avenida 6 de agosto. Esquina moxos numero 542<br>
+            cochabamba.<br>
+            Teléfonos: 4795260.<br>
+            </h6>
+        </div>
+
+        <div class="col-xs-12 col-md-6 text-right">
+            <h6 class="text-muted lead">ENCUENTRANOS EN LAS REDES</h6>
+            <div class="redes-footer">
+              <a href="https://www.facebook.com/"><img src="imagenes/facebook-2.png"></a>
+              <a href="https://twitter.com/"><img src="imagenes/twitter-2.png"></a>
+              <a href="https://www.youtube.com/"><img src="imagenes/youtube-2.png"></a>
+            </div>
+        </div>
+    </div>
+    <div class="row"> 
+
+        <div class="col-md-12 text-right">
+            <p class="text-muted small">José Miguel, arte y belleza @2016.<br> Todos los derechos reservados.</p>
+        </div>
+        
+    </div>
+
+    </div>
+  </div>  
+</div>
+</footer>
