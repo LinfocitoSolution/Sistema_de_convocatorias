e62d76f398569b1d8ffe3e08d1be82580f0b0eff
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-04-14 11:31:46 -0400

    funciones eloquent

--- resources/views/calls/login.blade.php
+++ resources/views/calls/login.blade.php
@@ -24,6 +24,7 @@
 @section("cabecera")
 @endsection
 @section("infoGeneral")
+
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,31 @@ Route::resource('noregister', 'CallController@noregister');
 Route::resource('login', 'CallController@login');
 Route::resource('formulariopost', 'CallController@formulariopost');
 Route::get("/leer",function(){//ruta del modelo
-    $articulos=App\Articulo::all();//namespace del modelo, importante
+   /* $articulos=App\Articulo::all();//namespace del modelo, importante
     foreach($articulos as $articulo)
     {
         echo "Nombre:  " .  $articulo->Nombre_Articulo . " <br> " . "precio: " . $articulo->Precio . "<br>";
-    }
+    }*/
+    $articulos=App\Articulo::where("Seccion","Ceramica")->min("precio");
+    return $articulos;
 });
+Route::get("/insertar",function(){//ruta del modelo
+    $articulos=new App\Articulo;
+    $articulos->Nombre_Articulo="pantalones";
+    $articulos->Precio="60";
+    $articulos->Pais_origen="bolivia";
+    $articulos->observaciones="lijado con lija";
+    $articulos->seccion="confeccion";
+   
+    $articulos->save();
+ });
+ Route::get("/actualizar",function(){//ruta del modelo
+    $articulos=App\Articulo::find(1240);
+    $articulos->Nombre_Articulo="pantalones";
+    $articulos->Precio="90";
+    $articulos->Pais_origen="bolivia";
+    $articulos->observaciones="lijado con lija";
+    $articulos->seccion="confeccion";
+   
+    $articulos->save();
+ });
