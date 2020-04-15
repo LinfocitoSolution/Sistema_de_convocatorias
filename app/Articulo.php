de63534f2e05146bed79248d09fc8af72c040213
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-04-15 05:24:52 -0400

    Revert "Revert "eloquent pruebas""
    This reverts commit b062080641c4ee8d6c6aa67bcd754d0c1cc04813.

--- app/Articulo.php
+++ app/Articulo.php
@@ -3,9 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Articulo extends Model
 {
+    use SoftDeletes;
+    protected $dates=['deleted_at'];
     //
     //protected $primaryKey="articulo";
+    /*protected $fillable=[
+        "Nombre_Articulo",
+        "Precio",
+        "Pais_Origen",              //moidificar el archivo del modelo para borrar
+        "Observaciones",
+        "Seccion"
+    ];*/
+    public function cliente()//join inverso del join entre cliente y articulo
+    {
+        return $this->belongsTo("App\Cliente");
+    }
 }
