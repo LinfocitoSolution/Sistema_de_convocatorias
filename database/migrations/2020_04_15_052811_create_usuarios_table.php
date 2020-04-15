cd78b11b39c5757586cf8780f4b57cc58990e475
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-04-15 06:17:53 -0400

    migracion_creacion_usuario

--- database/migrations/2020_04_15_052811_create_usuarios_table.php
+++ database/migrations/2020_04_15_052811_create_usuarios_table.php
@@ -15,6 +15,9 @@ class CreateUsuariosTable extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string("email");
             $table->string('NombreUsuario');
             $table->string('password');
             
