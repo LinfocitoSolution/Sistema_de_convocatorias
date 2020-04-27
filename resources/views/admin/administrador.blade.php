aa742ee80e5c5fc1b03f2ccd717e4c3d969f9810
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-04-27 07:40:18 -0400

    nombre de usuario en administrador

--- resources/views/admin/administrador.blade.php
+++ resources/views/admin/administrador.blade.php
@@ -85,7 +85,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Fulanito</a>
+          <a href="#" class="d-block">{{ ucfirst(Auth()->user()->nombre) }}</a>
         </div>
       </div>
 
