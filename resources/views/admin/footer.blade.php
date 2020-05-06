be99e28ad765060b1935db7ceb15f21577a2f5a2
YesbiMadelen <50107467+YesbiMadelen@users.noreply.github.com>
2020-05-06 16:59:38 -0400

    sidebar admin
    division

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,22 @@
+ <!-- Main Footer -->
+ <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Universidad Mayor de San Simon
+    </div>
+    <!-- Default to the left -->
+    <strong>Linfocitos &copy; 2014-2020 <a href="https://adminlte.io">LinfocitoSolution</a>.</strong> Todos los derechos reservados.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
