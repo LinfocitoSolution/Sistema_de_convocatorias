c4d6901d1e9a6ef219dedfc9167d079b5d5daadf
lucero <lucero18sistemas@gmail.com>
2020-05-19 17:19:50 -0400

    index y create de rol
    plantillas hechas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -119,10 +119,10 @@
 <div class="convocatoria">
   <div class="container">
     <form >
-      <div class="form-row">
+      <div class="form-row justify-content-around">
       
          @foreach ($convocatorias as $convocatoria)
-           <div class="col-sm-1 col-md-6 col-lg-6 col-xl-6">
+           <div class="col-12 col-sm-4 col-md-6 col-lg-6 col-xl-6">
                 <div class="card text-white bg-dark mt-5" style="width: 30rem;">
                     <img src="{{ asset('imagenes/web.JPG') }}" class="card-img-top" alt="laboratorios" width="200" height="200">
                         <div class="card-body">
