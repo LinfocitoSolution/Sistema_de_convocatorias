ca12c0c42512080f072b06cd645492905e1cc105
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-05-22 16:46:23 -0400

    Revert "Revert "Merge branch 'luce'""
    This reverts commit 22e32f52d62284f5879fb4c1fe009ff2162ef0fa.

---
+++ resources/views/admin/roles/edit.blade.php
@@ -0,0 +1,34 @@
+@extends("admin.layouts.plantilladmin")
+
+@section("title")
+    Create
+@endsection
+
+@section("content")
+ <!-- Content Wrapper. Contains contiene paginas -->
+ <div class="content-wrapper">
+    <div class="container">
+        <div class="row aling-items-center justify-content-center">
+            <div class="col-md-12">
+                <div class="card mt-5">
+                    <div class="card-header">
+                        <h1>Editar Rol</h1></div>
+                    <div class="card-body">
+                        <form class="form-horizontal">
+                            
+                            @include('admin.roles.partials.form')
+
+                            <div class="form-actions text-center">
+                                <button class="btn btn-outline-primary" type="submit">Actualizar</button>
+                                <a class="btn btn-outline-danger" href="{{ url('roles') }}">Cancelar</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  <!-- /.content-wrapper -->
+
+@endsection
