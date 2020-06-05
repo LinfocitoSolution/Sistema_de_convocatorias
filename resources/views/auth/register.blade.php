b3240454c56be15dfdf7a943b5882ac0468006a5
YesbiMadelen <50107467+YesbiMadelen@users.noreply.github.com>
2020-06-05 18:19:10 -0400

    color
    color en el navbar

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,7 +22,7 @@
 				<div class="d-flex justify-content-center h-100">
 					<div class="card">
 						<!--cabeza del formulario-->
-						<div class="card-header bg-info text-dark">
+						<div class="card-header  text-white">
 							<h3>Registro Postulante</h3>
 						</div>  
 						<!--fin de cabeza-->
@@ -35,7 +35,7 @@
 								<label for="validationTooltip01"class="text-black">Nombre completo</label>
 								<div class="input-group form-group">
 									<div class="input-group-prepend">
-										<span class="input-group-text px-3"><i class="fas fa-user-alt"></i></span>
+										<span class="input-group-text px-3"><i class="fas fa-user-alt icon-cog "></i></span>
 									<!--</div>
 									<input type="text" class="form-control text-capitalize" placeholder="Nombre" name="name" id="Nombre" value="{{ old('name') }}"> 
 								</div>
@@ -68,7 +68,7 @@
                                 <label for="validationTooltip02"class="text-black">Apellido completo</label>
 								<div class="input-group form-group">
 									<div class="input-group-prepend">
-										<span class="input-group-text bg-info px-3"><i class="fas fa-user-tie"></i></span>
+										<span class="input-group-text  px-3"><i class="fas fa-user-tie icon-cog"></i></span>
 									</div>
 
 									<input class="form-control {{ $errors->has('lastname') ? 'is-invalid' : '' }}"
@@ -85,7 +85,7 @@
 								<label for="validationTooltip03"class="text-black">Nombre usuario</label>
 								<div class="input-group form-group">
 									<div class="input-group-prepend">
-										<span class="input-group-text"><i class="fa fa-user-shield"></i></span>
+										<span class="input-group-text"><i class="fa fa-user-shield icon-cog"></i></span>
 									</div>
 
 									<input class="form-control {{ $errors->has('username') ? 'is-invalid' : '' }}"
@@ -102,7 +102,7 @@
 								<label for="validationTooltip04"class="text-black">Carrera</label>
 								<div class="input-group form-group">
 									<div class="input-group-prepend">
-										<span class="input-group-text"><i class="fa fa-graduation-cap"></i></span>
+										<span class="input-group-text"><i class="fa fa-graduation-cap icon-cog"></i></span>
 									</div>
 
 									<!--<input type="text" class="form-control text-capitalize" placeholder="Carrera" name="career" value="{ old('career') }}">-->
@@ -115,7 +115,7 @@
 								<label for="validationTooltip05"class="text-black">E-mail</label>
 								<div class="input-group form-group">
 									<div class="input-group-prepend">
-										<span class="input-group-text px-3"><i class="fa fa-at"></i></span>
+										<span class="input-group-text px-3"><i class="fa fa-at icon-cog"></i></span>
 									</div>
 									<input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
 									name="email"
@@ -130,7 +130,7 @@
 								<label for="validationTooltip06"class="text-black">Contraseña</label>
 								<div class="input-group form-group">
 									<div class="input-group-prepend">
-										<span class="input-group-text px-3"><i class="fas fa-key"></i></span>
+										<span class="input-group-text px-3"><i class="fas fa-key icon-cog"></i></span>
 									</div>
 									<input type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}"
 									name="password"
@@ -144,7 +144,7 @@
 								<label for="validationTooltip07"class="text-black">Confirma contraseña</label>
 								<div class="input-group form-group">
 									<div class="input-group-prepend">
-										<span class="input-group-text px-3"><i class="fas fa-key"></i></span>
+										<span class="input-group-text px-3"><i class="fas fa-key icon-cog"></i></span>
 									</div>
 
 									<input type="password" class="form-control {{ $errors->has('confirmpassword') ? 'is-invalid' : '' }}"
@@ -157,9 +157,9 @@
 								</div>
 
 									
-								<small id="passwordHelpBlock" class="form-text text-white mt-3"> La contraseña debe tener 8-25 caracteres, contener al menos una letra y  un numero, puede ser mayusculas , <br>no debe contener espacios, caracteres especiales, o emoji. </small>
+								<small id="passwordHelpBlock" class="form-text text-black mt-3"> La contraseña debe tener 8-25 caracteres, contener al menos una letra y  un numero, puede ser mayusculas , <br>no debe contener espacios, caracteres especiales, o emoji. </small>
 								
-								<button type="submit" class="btn btn-info rounded-pill active btn-block mt-3" > Registrar </button>
+								<button type="submit" class="btn  rounded-pill active btn-block mt-3" > Registrar </button>
 
 						  </form>
 						    <!--Inicio de pie formulario-->
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
     </div>       
 </div> 
 <!-- navbar -->
- <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-info">
+ <nav class="navbar sticky-top navbar-expand-lg">
   <a class="navbar-brand  text-white" href="{{url('/')}}" tabindex="-1" >Inicio</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
