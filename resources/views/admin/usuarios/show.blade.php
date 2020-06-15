30674c7fda3273f9aea5c55de9e455edb7129e76
YesbiMadelen <50107467+YesbiMadelen@users.noreply.github.com>
2020-06-15 16:31:37 -0400

    show.blade.php de users
    show de users

--- resources/views/admin/usuarios/show.blade.php
+++ resources/views/admin/usuarios/show.blade.php
@@ -12,7 +12,7 @@
         <!--CABEZA-->
            <div class="card-header">
      <!-----User { $user->id }}----->
-                <h1>carlos</h1>
+                <h1>User 
           </div> 
 
         <!---FIN CABEZA-->
@@ -21,18 +21,18 @@
                 <table class="table table-bordered table-striped table-hover">
                 <tbody>
                 <tr><!---------------{ $user->id }}--->
-                    <th>ID.</th><td>carlos</td>
+                    <th>Nombre</th><td>{{ old('name', isset($user) ? $user->name : '') }}</h1></td>
                 </tr>
                 <!-------{ trans('users.name') }}-->
-                <tr><th> nombre</th>
+                <th>Apellido</th><tr><th> {{ old('lastname', isset($user) ? $user->lastname : '') }}</th>
                     <!----{ $user->name }}--->
-                    <td> carlos </td></tr>
+                    <td>{{ old('username', isset($user) ? $user->username : '') }}</td></tr>
                  <!-----{ trans('users.email') }}---->   
-                <tr><th>E-mail  </th>
+                <tr><th>{{ old('email', isset($user) ? $user->email : '') }}  </th>
                     <!---{ $user->email }} --->
                     <td>carlos@gmail.com</td></tr>
                      <!-----{ trans('users.password') }} --->
-                <tr><th>contraseña </th>
+                <tr><th> </th>
                     <!---{ $user->password }} -->
                     <td> carlitos123</td></tr>
                </tbody>
