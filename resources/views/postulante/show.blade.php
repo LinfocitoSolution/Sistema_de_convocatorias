f230f9b0187fc89499641e3ea9e65041a4928885
lucero <lucero18sistemas@gmail.com>
2020-06-18 17:07:35 -0400

    añadir botion atras

--- resources/views/postulante/show.blade.php
+++ resources/views/postulante/show.blade.php
@@ -34,14 +34,20 @@
                 <!----fin del cuerpo perfil--->
                  <!----INICIO DE PIE-->   
             <div class="card-footer">
+                @if(Auth::user()->roles->first()->name=='Postulante')
                   <div class="text-center">
-                        <a class="btn btn-outline-dark  text-white m-2 my-sm-2"  href="{{ route('postulante.edit', $user->id) }}"><i class="fa fa-edit mr-2"></i>Editar</a>
-                  
-                    <a class="btn btn-outline-dark  text-white m-2 my-sm-2"  href="{{url('enviar_resetPassword')}}"><i class="fa fa-key mr-2"></i>Cambiar Contraseña</a>
-                    
+                        <a class="btn btn-outline-dark  text-white m-2 my-sm-2" data-toggle="tooltip" data-placement="left" title="Presiona el botón para EDITAR" href="{{ route('postulante.edit', $user->id) }}"><i class="fa fa-edit mr-2"></i>Editar</a>
+                        <a class="btn btn-outline-dark  text-white m-2 my-sm-2" data-toggle="tooltip" data-placement="top" title="El botón no esta disponible" href="#"><i class="fa fa-key mr-2"></i>Cambiar Contraseña</a>
+                        <a class="btn btn-outline-dark text-white m-2" data-toggle="tooltip" data-placement="right" title="Presiona el botón para volver a la pantalla principal" href="{{url('/')}}"><i class="fa fa-arrow-left mr-2"></i>Atrás</a>
                 </div>
-           
-            </div>
+               @else
+               <div class="text-center">
+                   <a class="btn btn-outline-dark  text-white m-2 my-sm-2" data-toggle="tooltip" data-placement="left" title="Presiona botón para EDITAR" href="{{ route('postulante.edit', $user->id) }}"><i class="fa fa-edit mr-2"></i>Editar</a>
+                   <a class="btn btn-outline-dark  text-white m-2 my-sm-2" data-toggle="tooltip" data-placement="top" title="El botón no esta disponible" href="#"><i class="fa fa-key mr-2"></i>Cambiar Contraseña</a>
+                   <a class="btn btn-outline-dark text-white m-2" data-toggle="tooltip" data-placement="right" title="Presiona el botón para volver alpanel de trabajo" href="{{url('administrador')}}"><i class="fa fa-arrow-left mr-2"></i>Atrás</a>
+              </div>
+              @endif
+            </div>  
              <!---FIN DE PIE-->
 @endsection		
 	
