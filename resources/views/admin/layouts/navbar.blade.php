e809ab8413d7bad0b746c841e6c6f68145d8dfc6
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-06-18 16:01:48 -0400

    Update navbar.blade.php

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -21,7 +21,7 @@
           <button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown">Administrador</button>
             <div class="dropdown-menu">
               <div class="dropdown-divider"></div>
-                <a class="dropdown-item bg-dark" tabindex="0" data-toggle="tooltip" data-placement="left" data-trigger="hover" title="Este boton no esta disponible" href="#">
+                <a class="dropdown-item bg-dark" tabindex="0" data-toggle="tooltip" data-placement="left" data-trigger="hover" title="Este boton no esta disponible" href="{{ route('postulante.show',ucfirst(Auth::user()->id))}}">
                   <i class="fas fa-user mr-2"></i>Perfil
                 </a>
                  <div class="dropdown-divider"></div>
