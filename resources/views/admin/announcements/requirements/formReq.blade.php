ecd952362e9a89b6351b0cde33d0910e9d6d2ee6
YesbiMadelen <50107467+YesbiMadelen@users.noreply.github.com>
2020-06-24 15:44:23 -0400

    crear vista de crud de requerimientos
    editReq, fromReq,createReq

---
+++ resources/views/admin/announcements/requirements/formReq.blade.php
@@ -0,0 +1,57 @@
+<div class="form-row">
+
+    <!----nombre-->
+        <div class="col-md-6 mb-3">
+            <label class="col-form-label" for="name">Item</label>
+            <select name="career" class="custom-select form-control">
+                <option selected class="text-muted"value="sistemas">1</option>
+                <option value="informatica">2</option> 
+                <option value="electrónica">3</option>
+                <option value="informatica">4</option> 
+                <option value="electrónica">5</option>
+                <option value="informatica">6</option> 
+                <option value="electrónica">7</option>
+                <option value="informatica">8</option> 
+                <option value="electrónica">9</option>
+          </select>
+        </div>
+    
+   <!--Cantidad de auxiliares-->
+   <div class="col-md-6 mb-3">
+    <label class="col-form-label" for="">Cantidad de Auxiliares</label>
+      <span class="input-group-append">
+            <button class="btn btn-dark" type="button">CA</button>
+        </span>
+    </div>
+
+<!--Horas académicas-->
+<div class="col-md-6 mb-3">
+    <label class="col-form-label" for="">Horas Académicas</label>
+    <span class="input-group-append">
+        <button class="btn btn-dark" type="button">HA</button>
+    </span>
+</div>
+<!--Nombre de auxiliatura-->
+<div class="col-md-6 mb-3">
+    <label class="col-form-label" for="">Nombre de Auxiliatura</label>
+    <span class="input-group-append">
+       <button class="btn btn-dark" type="button">NA</button>
+    </span>
+</div>
+<!--Código de auxiliatura-->
+<div class="col-md-6 mb-3">
+    <label class="col-form-label" for="">Código de Auxiliatura</label>
+    <span class="input-group-append">
+        <button class="btn btn-dark" type="button">CA</label>
+        </span>
+    </div>
+</div>
+    
+    
+
+
+
+
+            
+
+   
---
+++ resources/views/admin/announcements/requirements/partials/createReq.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.plantilladmin')
+
+@section('htmlheader_title')
+    Requerimiento
+@endsection
+
+@section('content')
+<div class="content-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <i class="fa fa-user-edit"></i>Crear Requerimiento</div>
+
+                         
+                           
+                    <div class="card-body">
+                        <form class="form-horizontal" action="{{}}" method="POST">
+                            
+
+                            @include('admin.usuarios.form')
+
+                            <div class="form-actions text-center">
+                                <button class="btn btn-outline-dark" type="submit">Guardar</button>
+                            <a class="btn btn-outline-dark" href="{{}}">Cancelar</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-->
+        </div>
+        <!-- /.row-->
+    </div>
+</div>
+
+@endsection
+
