aeb05dab38e78b08b10332bf4b66339ab990c746
cristiancry <50432278+cristiancry@users.noreply.github.com>
2020-06-25 16:43:06 -0400

    Update sidebar.blade.php

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -137,7 +137,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                 <a href="" class="nav-link">
+                 <a href="{{route('requerimientos.create')}}" class="nav-link">
                   <i class="fas fa-plus-square nav-icon"></i>
                   <p>Nuevo Requerimiento</p>
                 </a>
