6ccf2611f066dfb4efaf089570ec264453267a5e
lucero <lucero18sistemas@gmail.com>
2020-07-03 17:41:16 -0400

    pequeños cambios a convocatoria

--- resources/views/admin/announcements/plantilla/generar_convocatoria.blade.php
+++ resources/views/admin/announcements/plantilla/generar_convocatoria.blade.php
@@ -17,7 +17,7 @@
            <h3 style="margin-left: 12cm">GESTION 2020</h3>
            
          <br>
-        <p>El Departamento de Informática y Sistemas junto a las Carreras de Ing. Informática e Ing. 
+        <p class="parrafo" style="text-align:justify">El Departamento de Informática y Sistemas junto a las Carreras de Ing. Informática e Ing. 
            De  Sistemas  de  la  Facultad  de  Ciencias  y  Tecnología,  convoca  al  concurso  de  méritos  y 
            examen de competencia para la provisión de Auxiliares del Departamento, tomando como 
            base los requerimientos que se tienen programados para la gestión 2020.</p>  
@@ -37,30 +37,26 @@
   
            </div>
        <br>
-        <p><b>NOTA.-</b>Toda la documentación se legalizará gratuitamente en Secretaria del Departamento 
+        <p class="parrafo" style="text-align:justify"><b>NOTA.-</b>Toda la documentación se legalizará gratuitamente en Secretaria del Departamento 
            de  Informática  y  Sistemas.  (Presentar  original  y  fotocopias).  La  documentación  no  será 
            devuelta.</p> 
        <br>
        <h4>4.&nbsp; &nbsp; FECHA Y LUGAR DE PRESENTACIÓN DE DOCUMENTOS </h4>
        <h4>4.1.&nbsp;DE LA FORMA</h4>
-       <p>&nbsp;Presentación de la documentación en sobre manila cerrado y rotulado con: </p>
-       <p>
-            &nbsp;&nbsp;&nbsp;    - Nombre y apellidos completos, dirección, teléfono(s) y e-mail del postulante, 
-        <br>&nbsp;&nbsp;&nbsp; - Código(s) de item de la(s) auxiliatura(s) a la(s) que se postula. 
-        <br>&nbsp;&nbsp;&nbsp; - Nombre(s) de la(s) auxiliatura(s) a la(s) que sepresenta. 
-      </p>
-      <br>
-      <h4>5.&nbsp;&nbsp;CALIFICACION DE MERITOS</h4>
-      <p>La calificación de méritos se basará en los documentos presentados por el postulante y se 
-         realizará sobre la base de 100 puntos que representa el 20% del puntaje final y se ponderará 
-         de la siguiente manera. </p>
-      <br>
-      <p><b>NOTA.-</b>Todo certificado será ponderado hasta el valor delpuntaje especificado en la tabla.</p>
-      <h4>6.&nbsp;&nbsp;CALIFICACIÓN DE CONOCIMIENTOS</h4> 
-      <p>La  calificación  de  conocimientos  se  realiza  sobre  la  base  de  100  puntos,  equivalentes  al  80%  de  la 
-         calificación final.</p> 
-      <p>Las  pruebas  para  los  auxiliares  sobre  conocimientos se  realizarán  de  acuerdo  al  temario  y  tabla 
-         siguiente.</p> 
+       <p  class="parrafo">Presentación de la documentación en sobre manila cerrado y rotulado con:</p>
+       <p  class="parrafo">- Nombre y apellidos completos, dirección, teléfono(s) y e-mail del postulante.</p> 
+       <p  class="parrafo">- Código(s) de item de la(s) auxiliatura(s) a la(s) que se postula.</p> 
+       <p  class="parrafo">- Nombre(s) de la(s) auxiliatura(s) a la(s) que sepresenta.</p>
+       <br>
+        <h4>5.&nbsp;&nbsp;CALIFICACION DE MERITOS</h4>
+        <p  class="parrafo" style="text-align:justify">La calificación de méritos se basará en los documentos presentados por el postulante y se 
+           realizará sobre la base de 100 puntos que representa el 20% del puntaje final y se ponderará 
+           de la siguiente manera.</p>
+        <br>
+        <p  class="parrafo" style="text-align:justify"><b>NOTA.-</b>Todo certificado será ponderado hasta el valor delpuntaje especificado en la tabla.</p>
+        <h4>6.&nbsp;&nbsp;CALIFICACIÓN DE CONOCIMIENTOS</h4> 
+        <p  class="parrafo" style="text-align:justify">La  calificación  de  conocimientos  se  realiza  sobre  la  base  de  100  puntos,  equivalentes  al  80%  de  la calificación final.</p> 
+        <p  class="parrafo" style="text-align:justify">Las  pruebas  para  los  auxiliares  sobre  conocimientos se  realizarán  de  acuerdo  al  temario  y  tabla siguiente.</p> 
     </div> 
     {{-- FIN DE LA PRIMERA PARTE --}}
     <div id = "parte2"> 
@@ -69,7 +65,7 @@
       <br>
       <div id = "elementH"></div>
       <h4> 7.&nbsp;&nbsp; DE LOS TRIBUNALES </h4>
-      <p>Los Honorables Consejos de Carrera de Informática ySistemas designarán respectivamente; para la 
+      <p  class="parrafo" style="text-align:justify">Los Honorables Consejos de Carrera de Informática ySistemas designarán respectivamente; para la 
          calificación de méritos 1 docente y 1 delegado estudiante, de la misma manera para la comisión de 
          conocimientos cada consejo designará 1 docente y unestudiante veedor por cada temática.</p> 
       <br>
@@ -87,16 +83,16 @@
       <br>
     </div>{{--FIN SEGUNDA PARTE--}}
     <div id="firma">
-         <p id="nombre1" class="fi"> Lic. Henrry Villarroel Tapia</p> 
-         <p id = "nombre2"class="fi"> Lic. Yony Montoya Brugos </p>
-         <p id="sistemas" class="fi" style="margin-left: 7cm; margin-top:5cm; display: inline">DIR. ING. SISTEMAS</p> 
-         <p id="informatica" class="fi" style="margin-left: 20cm; margin-top:-2cm; display: inline"" > ING. INFORMATICA</p>   
+         <p id="nombre1" class="parrafo"> Lic. Henrry Villarroel Tapia</p> 
+         <p id = "nombre2"class="parrafo"> Lic. Yony Montoya Brugos </p>
+         <p id="sistemas" class="parrafo" style="margin-left: 7cm; margin-top:5cm; display: inline">DIR. ING. SISTEMAS</p> 
+         <p id="informatica" class="parrafo" style="margin-left: 20cm; margin-top:-2cm; display: inline" > ING. INFORMATICA</p>   
          <br>
-         <p id="nombre3" class="fi">Ing. Jimmy Villarroel Novillo </p> 
-         <p id="infoysis" class="fi"><b>DPTO INFORMATICA Y SISTEMAS</b></p>
+         <p id="nombre3" class="parrafo">Ing. Jimmy Villarroel Novillo </p> 
+         <p id="infoysis" class="parrafo"><b>DPTO INFORMATICA Y SISTEMAS</b></p>
          <br>
-         <p id="nombre4" class="fi">Ing. Alfredo Cosio Papadopolis</p> 
-         <p id ="decano" class="fi"><b>DECANO - FCyT</b></p>
+         <p id="nombre4" class="parrafo">Ing. Alfredo Cosio Papadopolis</p> 
+         <p id ="decano" class="parrafo"><b>DECANO - FCyT</b></p>
     </div>      
  </body>
     <a href="javascript:toPDF()">PDF</a>
