1a72cd2a942a50748a1ab4f60b72bb755ce477b9
YesbiMadelen <50107467+YesbiMadelen@users.noreply.github.com>
2020-07-07 18:14:10 -0400

    vistas y rutas
    view create1 edit1 form1

---
+++ resources/views/admin/announcements/edit1.blade.php
@@ -0,0 +1,48 @@
+@extends("admin.layouts.plantilladmin")
+
+@section('title')
+    Nueva Convocatoria de Docencia
+@endsection
+@section("content")
+ <!-- Content Wrapper. Contains contiene paginas -->
+<div class="content-wrapper">
+    
+       @if (count($errors) > 0)
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+    @endif
+    <div class="container">
+      <div class="row">
+       <div class="col-sm-12">
+        <div class="card mt-2"> 
+          <div class="card-header">
+            <h1>Editar convocatoria de Docencia</h1>
+          </div>
+          <div class="card-body">
+  <div class="medio">
+        <form class="form-group" method="POST" action="/call/{{$call->id}}" enctype="multipart/form-data">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          {{ method_field('PUT') }}
+          
+            @include('admin.announcements.form')
+                   <br>
+                   
+                   <div class="form-actions text-center">
+                    <button class="btn btn-outline-dark" type="submit">Guardar</button>
+                    <a class="btn btn-outline-dark" href="{{route('call.index') }}">Cancelar</a>
+                  </div>  
+        </form> 
+      </div> 
+        </div>
+       </div>
+      </div>
+    </div>      
+   </div>      
+</div>
+  <!-- /.content-wrapper -->
+@endsection
