03bb614f004ca1b5d8cf60bfbb56210ce00ef86c
YesbiMadelen <50107467+YesbiMadelen@users.noreply.github.com>
2020-08-10 18:19:49 -0400

    (*)
    en register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -53,13 +53,13 @@
 						   <!--fin de cabeza-->
 					          <!--Inicio cuerpo de formulario-->  
 						          <div class="card-body">
-					                
+					                <p class="text-danger">(*)Campo obligatorio</p>
 							           <form name="fregistro" class="form-group" method="POST" action={{url("/register")}}  id="formReg" autocomplete="off" >                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       	 
 								           <input type="hidden" name="_token" value="{{ csrf_token() }}">
 									     <div class="form-row" > 
 										          <!--campo nombre-->
 								                     <div class="col-md-6 mb-2">
-								                         <label for="validationTooltip01"class="text-black">Nombre completo</label>
+								                         <label for="validationTooltip01"class="text-black"><b class="text-danger">(*)</b>Nombre completo</label>
 								                         <div class="input-group" data-html="true" data-toggle="popover" title="Restricciones" data-content="-M&aacute;ximo 50 caracteres <br> -M&iacute;nimo 3 caracteres <br> -No acepta caracteres especiales" >
 									                         <div class="input-group-prepend">
 										                         <span class="input-group-text px-3"><i class="fas fa-user-alt icon-cog "></i></span>
@@ -76,7 +76,7 @@
 												   <!--fin campo nombre-->  
 								                   <!--campo apellido-->
 								                      <div class="col-md-6 mb-2">
-                                                              <label for="validationTooltip02"class="text-black">Apellido completo</label>
+                                                              <label for="validationTooltip02"class="text-black"><b class="text-danger">(*)</b>Apellido completo</label>
 								                            <div class="input-group form-group" data-html="true" data-toggle="popover" title="Restricciones" data-content="-M&aacuteximo 50 caracteres <br> -No acepta caracteres especiales <br>-No acepta n&uacute;meros">
 									                             <div class="input-group-prepend">
 										                             <span class="input-group-text  px-3"><i class="fas fa-user-tie icon-cog"></i></span>
@@ -94,7 +94,7 @@
 											      <!----fin campo apellido--->		  		 							
 									              <!--campo nombre de usuario -->
 									                 <div class="col-md-6 mb-2">
-								                          <label for="validationTooltip03" class="text-black">Nombre usuario</label>
+								                          <label for="validationTooltip03" class="text-black"><b class="text-danger">(*)</b>Nombre usuario</label>
 								                             <div class="input-group form-group" data-html="true" data-toggle="popover" title="Restricciones" data-content="-M&aacute;ximo 20 caracteres <br>-Solo se permite alfanum&eacute;rico <br>-No acepta espacios <br>-Se permite may&uacute;sculas">
 									                             <div class="input-group-prepend">
 										                             <span class="input-group-text"><i class="fa fa-user-shield icon-cog"></i></span>
@@ -111,7 +111,7 @@
 												  <!--fin campo usuario--->	   									
 									              <!--campo carrera--->
 									                 <div class="col-md-6 mb-2">
-								                             <label for="validationTooltip04"class="text-black">Carrera</label>
+								                             <label for="validationTooltip04"class="text-black"><b class="text-danger">(*)</b>Carrera</label>
 								                         <div class="input-group form-group" data-html="true" data-toggle="popover" title="Restricciones" data-content="selecciona la carrera que est&aacute; cursando">
 									                         <div class="input-group-prepend">
 										                          <span class="input-group-text"><i class="fa fa-graduation-cap icon-cog"></i></span>
@@ -127,7 +127,7 @@
 												  <!---fin campo carrera--> 								
 									              <!--campo email-->
 									                      <div class="col-md-6 mb-2">
-								                             <label for="validationTooltip05"class="text-black">E-mail</label>
+								                             <label for="validationTooltip05"class="text-black"><b class="text-danger">(*)</b>E-mail</label>
 								                             <div class="input-group form-group" data-html="true" data-toggle="popover" title="Restricciones" data-content="Sigue el ejemplo">
 									                              <div class="input-group-prepend">
 										                             <span class="input-group-text px-3"><i class="fa fa-at icon-cog"></i></span>
@@ -144,7 +144,7 @@
 									               <!---fin campo email--->
 									                <!--campo contraseña-->
 									                   <div class="col-md-6 mb-2">
-								                           <label for="validationTooltip06"class="text-black">Contraseña</label>
+								                           <label for="validationTooltip06"class="text-black"><b class="text-danger">(*)</b>Contraseña</label>
 								                            <div class="input-group form-group" data-html="true" data-toggle="popover" title="Restricciones" data-content="-M&aacute;ximo 25 caracteres <br>-M&iacute;nimo 8 caracteres <br>-No permite caracteres especiales <br>-Debe ingresar al menos una letra y un n&uacute;mero<br>-No permite espacios">
 									                            <div class="input-group-prepend">
 										                           <span class="input-group-text px-3"><i class="fas fa-key icon-cog"></i></span>
@@ -161,7 +161,7 @@
 												    <!---fin campo contraseña--->
 									                <!--campo confirmar contraseña-->
 									                    <div class="col-md-6 mb-2">
-								                              <label for="validationTooltip07"class="text-black">Confirma contraseña</label>
+								                              <label for="validationTooltip07"class="text-black"><b class="text-danger">(*)</b>Confirma contraseña</label>
 								                             <div class="input-group form-group" data-html="true" data-toggle="popover" title="Restricciones" data-content="confirme la contraseña creada anteriormente">
 									                             <div class="input-group-prepend">
 										                              <span class="input-group-text px-3"><i class="fas fa-key icon-cog"></i></span>
