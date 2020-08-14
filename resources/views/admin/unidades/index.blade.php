7aee4a954cc442c048a5d012abe1f8cdc9743b60
RichardVasquezLia <richardvasquezlia@gmail.com>
2020-08-13 20:26:53 -0400

    Revert "restricción de unidad"
    This reverts commit 17ee70e79ccdf9a73af917aa524be4c95d6d0dbd.

--- resources/views/admin/unidades/index.blade.php
+++ resources/views/admin/unidades/index.blade.php
@@ -16,7 +16,6 @@
                     <i class="fa fa-user-plus"></i>
                 </a>
             </div>
-            
             <div class="card-body">
                 <table class="table table-bordered table-striped table-sm">
                     <thead>
@@ -36,21 +35,19 @@
                                     <a class="btn btn-dark btn-sm mx-1 my-1" data-toggle="tooltip" data-trigger="hover" title="presiona para editar una unidad" href="{{route('unidades.edit', $unidad) }}">
                                         <i class="fa fa-pencil-alt"></i>
                                     </a> 
-                                    @if (!App\Convocatoria::where('unit_id','=', $unidad->id)->exists())
-                                        <form action="{{route('unidades.destroy',$unidad->id)}}" method="POST" style="display:inline-block;">
-                                            {{ csrf_field() }}                                                                                                                     
-                                            {{ method_field('DELETE') }}                                                                                                                                                
-                                            <button class="btn btn-dark btn-sm mx-1 my-1" data-toggle="tooltip" data-trigger="hover" title="presiona para eliminar una unidad" type="submit" margin-left="50" onclick="return confirm('Está seguro de eliminar la unidad?')" >
-                                                <i class="fa fa-trash-alt"></i>                                
-                                            </button>                            
-                                        </form>
-                                    @endif
+                                    
+                                    <form action="{{route('unidades.destroy',$unidad->id)}}" method="POST" style="display:inline-block;">
+                                        {{ csrf_field() }}                                                                                                                     
+                                        {{ method_field('DELETE') }}                                                                                                                                                
+                                        <button class="btn btn-dark btn-sm mx-1 my-1" data-toggle="tooltip" data-trigger="hover" title="presiona para eliminar una unidad" type="submit" margin-left="50" onclick="return confirm('Está seguro de eliminar la unidad?')" >
+                                            <i class="fa fa-trash-alt"></i>                                
+                                        </button>                            
+                                    </form>
                                 </td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
-                <p class="text-danger">*Nota: Para eliminar una unidad previamente no debe tener convocatorias asociadas a la misma</p>
             </div>
         </div>
     </div>  
